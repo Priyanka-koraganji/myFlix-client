53dc00cd06ce8062d123f91dc8460ca1e58ebb02
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-06-27 23:13:24 -0700

    Update login-view.jsx
    added react bootstrap code for login page

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Form, Button, Container, Row, Col, Card } from 'react-bootstrap';
+import './login-view.scss';
 
 export function LoginView(props) {
     const [username, setUsername] = useState('');
@@ -8,23 +10,37 @@ export function LoginView(props) {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(username, password);
-        /* Send a request to the server for authentication */
-        /* then call props.onLoggedIn(username) */
+        // Send a request to the server for authentication, then call props.onLoggedIn(username)
         props.onLoggedIn(username);
-    };
-
+    }
     return (
-        <form>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-            </label>
-            <button type="submit" onClick={handleSubmit}>Submit</button>
-        </form>
+        <Container className='login-view'>
+            <Row>
+                <Col></Col>
+                <Col md={4}>
+                    <h2>Login</h2>
+                    <Card>
+                        <Card.Body>
+                            <Form>
+                                <Form.Group className="mb-3" controlId="formUsername">
+                                    <Form.Label>Username:</Form.Label>
+                                    <Form.Control type="text" onChange={e => setUsername(e.target.value)} />
+                                </Form.Group>
+
+                                <Form.Group className="mb-3" controlId="formPassword">
+                                    <Form.Label>Password:</Form.Label>
+                                    <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
+                                </Form.Group>
+                                <Button variant="primary" type="submit" onClick={handleSubmit}>
+                                    Submit
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col></Col>
+            </Row>
+        </Container>
     );
 }
 LoginView.propTypes = {
