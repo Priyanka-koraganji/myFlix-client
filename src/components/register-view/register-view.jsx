95e12e114c56734e3d9c9bff46b0e15f575a5f76
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-06-27 23:15:17 -0700

    Update register-view.jsx
    imported react bootstrap

--- src/components/register-view/register-view.jsx
+++ src/components/register-view/register-view.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import './register-view.scss';
 
 export function RegisterView(props) {
 
@@ -16,25 +17,36 @@ export function RegisterView(props) {
     };
 
     return (
-        <form>
-            <div>
-                <label>Username:</label>
-                <input type='text' value={username} onChange={(e) => enterUsername(e.target.value)} />
-            </div>
-            <div>
-                <label>Password:</label>
-                <input type='password' value={password} onChange={(e) => enterPassword(e.target.value)} />
-            </div>
-            <div>
-                <label>Email:</label>
-                <input type='email' value={email} onChange={(e) => enterEmail(e.target.value)} />
-            </div>
-            <div>
-                <label>Birthday:</label>
-                <input type='date' value={birthday} onChange={(e) => enterBirthday(e.target.value)} />
-            </div>
-            <button type="submit" onClick={handleSubmit}>Register</button>
-        </form>
+        <Container className='register-view'>
+            <Row>
+                <Col></Col>
+                <Col md={8}>
+
+                    <Form>
+                        <h2>Welcome to Registration Page!</h2>
+                        <Form.Group  className="mb-3">
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control type='text' value={username} onChange={(e) => enterUsername(e.target.value)} required placeholder='Enter a username' />
+                        </Form.Group>
+                        <Form.Group  className="mb-3">
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control type='password' value={password} onChange={(e) => enterPassword(e.target.value)} required minLength="8" placeholder='Enter Password' />
+                        </Form.Group>
+                        <Form.Group  className="mb-3">
+                            <Form.Label>Email:</Form.Label>
+                            <Form.Control type='email' value={email} onChange={(e) => enterEmail(e.target.value)} required placeholder='Enter Email' />
+                        </Form.Group>
+                        <Form.Group  className="mb-3">
+                            <Form.Label>Birthday:</Form.Label>
+                            <Form.Control type='date' value={birthday} onChange={(e) => enterBirthday(e.target.value)} />
+                        </Form.Group>
+                        <Button variant="primary" type="submit" onClick={handleSubmit}>Register</Button>
+                    </Form>
+
+                </Col>
+                <Col></Col>
+            </Row>
+        </Container>
     )
 }
 RegisterView.propTypes = {
