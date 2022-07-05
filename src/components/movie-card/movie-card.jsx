2c4973954081ee34898ad8dd6e6fa8e43807b9b5
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-07-04 21:11:22 -0700

    Update movie-card.jsx
    created card element

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import './movie-card.scss';
+import { Link } from "react-router-dom";
 
 export class MovieCard extends React.Component {
     render() {
-        const { movie, onMovieClick } = this.props;
+        const { movie } = this.props;
 
         return (
             <Card className='movie-card'>
-                <Card.Img variant="top" src={movie.ImagePath} />
-                <Card.Body>
-                    <Card.Title>{movie.Title}</Card.Title>
-                    <Card.Text>{movie.Description}</Card.Text>
-                    <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+                <Card.Img variant="top" className='card-img' src={movie.ImagePath} />
+                <Card.Body className='card-body'>
+                    <Card.Title style={{ fontSize: '1em' }}>{movie.Title}</Card.Title>
+                    {/* <Card.Text>{movie.Description}</Card.Text> */}
+                    <Link to={`/movies/${movie._id}`}>
+                        <Button variant="primary link">See More</Button>
+                    </Link>
                 </Card.Body>
             </Card>
         );
@@ -31,6 +34,5 @@ MovieCard.propTypes = {
         Director: PropTypes.shape({
             Name: PropTypes.string.isRequired,
         })
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
+    }).isRequired
 };
