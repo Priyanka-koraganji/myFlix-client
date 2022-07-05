4afe43661740dcb17beee88acbcd9c80fe2fd14c
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-07-04 21:10:09 -0700

    Create favorite-movies.jsx
    created fav movies component

---
+++ src/components/profile-view/favorite-movies.jsx
@@ -0,0 +1,68 @@
+import React, { Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+import { Button, Card, Col } from 'react-bootstrap';
+
+import './profile-view.scss';
+
+export function FavoriteMovies(props) {
+    const { movies, favoriteMovies, currentUser, token } = props;
+    const favoriteMoviesList = [];
+
+    favoriteMovies.forEach(id => {
+        movies.map((movie) => {
+            if (movie._id === id) {
+                console.log(movie);
+                favoriteMoviesList.push(movie);
+            }
+        })
+
+    })
+
+    const handleMovieDelete = (movieId) => {
+        axios.delete(`https://yourmoviescollection.herokuapp.com/users/${currentUser}/movies/${movieId}`, {
+            headers: { Authorization: `Bearer ${token}` }
+        })
+            .then(() => {
+                alert(`The movie was successfully deleted.`)
+                window.open('/users/:username', '_self');
+            }).
+            catch(error => console.error(error))
+    }
+
+    return (
+        <Fragment>
+            {favoriteMoviesList.length === 0 ? (
+                <p>You have no favourite movies yet.</p>
+            ) : (
+                favoriteMoviesList.map((movie) => {
+                    return (
+                        <Col xs={10} sm={8} md={6} lg={4} >
+                            <Card id="movie-card">
+                                <Link to={`/movies/${movie._id}`}>
+                                    <Card.Img variant="top" src={movie.ImagePath} />
+                                </Link>
+                                <Card.Body>
+                                    <Card.Title>{movie.Title}</Card.Title>
+                                    <Card.Text>{movie.Description}</Card.Text>
+                                    <Link to={`/movies/${movie._id}`}>
+                                        <Button className="button" variant="outline-primary" size="sm">Open</Button>
+                                    </Link>
+                                    <Button
+                                        className="button ml-2"
+                                        variant="outline-primary"
+                                        size="sm" onClick={() => { handleMovieDelete(movie._id) }} >
+                                        Remove
+                                    </Button>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )
+                })
+            )
+            }
+        </Fragment>
+    )
+}
