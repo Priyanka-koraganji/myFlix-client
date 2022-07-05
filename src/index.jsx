8f29eafd5f885e370413354f4304cb1075f86eec
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-07-04 21:06:20 -0700

    Update index.jsx
    included store,connect

--- src/index.jsx
+++ src/index.jsx
@@ -2,24 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainView from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import './index.scss'
+import { createRoot } from 'react-dom/client';
 
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
+
+const store = createStore(moviesApp);
 
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
     render() {
         return (
-            <Container>
-                <MainView />
-            </Container>
-
+            <Provider store={store}>
+                <Container>
+                    <MainView />
+                </Container>
+            </Provider>
         );
     }
 }
+
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
 
 // Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+//ReactDOM.render(React.createElement(MyFlixApplication), container);
+
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<MyFlixApplication tab="home" />);
