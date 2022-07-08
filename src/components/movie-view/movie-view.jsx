a25fb773d51cc05b080227fc376ecd2c39d16477
Priyanka-koraganji <51729374+Priyanka-koraganji@users.noreply.github.com>
2022-07-07 21:10:44 -0700

    Update movie-view.jsx
    added favorite button

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,14 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './movie-view.scss';
 import { Link } from "react-router-dom";
-import { Button, Figure, Card } from 'react-bootstrap';
+import { Button, Figure, Card, Image } from 'react-bootstrap';
+import axios from 'axios';
 
 export class MovieView extends React.Component {
 
     keypressCallback(event) {
         console.log(event.key);
     }
-
+    FavMovie = () => {
+        const token = localStorage.getItem('token');
+        const { user, movie } = this.props;
+        console.log(movie._id);
+        axios.post(`https://yourmoviescollection.herokuapp.com/users/${user}/movies/${movie._id}`, {},
+            {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+            .then(res => {
+                console.log(res);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
     componentDidMount() {
         document.addEventListener('keypress', this.keypressCallback);
     }
@@ -18,7 +33,7 @@ export class MovieView extends React.Component {
         document.removeEventListener('keypress', this.keypressCallback);
     }
     render() {
-        const { movie, onBackClick } = this.props;
+        const { movie, onBackClick, user } = this.props;
 
         return (
             <>
@@ -31,26 +46,26 @@ export class MovieView extends React.Component {
                         <div className="movie-genre">
                             <span className="label"><strong>Genre:</strong></span>
                             <Link to={`/genre/${movie.Genre.Name}`}>
-                                <span className="value">{movie.Genre.Name}</span>
+                                <span className="value genre-link">{movie.Genre.Name}</span>
                             </Link>
                         </div>
                         <div className="movie-director">
                             <span className="label"><strong>Director:</strong></span>
                             <Link to={`/directors/${movie.Director.Name}`}>
-                                <span className="value">{movie.Director.Name}</span>
+                                <span className="value director-link" >{movie.Director.Name}</span>
                             </Link>
                         </div>
                         <div className="movie-description">
                             <span className="label"><strong>Description:</strong> </span>
                             <span className="value description">{movie.Description}</span>
                         </div>
-                        <Button className="back-button" variant="secondary" onClick={() => { onBackClick(); }}>Back</Button>
+                        <div className='buttons'>
+                            <Button className="back-button" variant="danger" onClick={() => { onBackClick(); }}>Back</Button>
+                            <Button className="Fav-button" variant="danger" onClick={this.FavMovie}>Add to Favorites</Button>
+                        </div>
                     </div>
-                    <div className="movie-poster">
-                        <Card>
-                            <Card.Img className="poster" src={movie.ImagePath} />
-                        </Card>
-                    </div>
+                    <img className="movie-poster" src={movie.ImagePath} />
+
                 </div>
 
 
